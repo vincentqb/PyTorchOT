9c77bfb1f28bbb42137194d889e0b0a90e32622d
Vincent Quenneville-Belair <vincentqb@gmail.com>
2024-11-26 17:02:54 -0500

    fix https://github.com/rythei/PyTorchOT/pull/4

--- ot_pytorch.py
+++ ot_pytorch.py
@@ -27,7 +27,7 @@ def sink(M, reg, numItermax=1000, stopThr=1e-9, device="cpu"):
         if cpt % 10 == 0:
             # we can speed up the process by checking for the error only all the 10th iterations
             transp = u.view(-1, 1) * (K * v)
-            err = (torch.sum(transp) - b).norm(2).pow(2).detach().cpu().item()
+            err = (torch.sum(transp, dim=0) - b).norm(2).pow(2).detach().cpu().item()
 
         cpt += 1
 
@@ -84,7 +84,7 @@ def sink_stabilized(M, reg, numItermax=1000, tau=1e2, stopThr=1e-9, warmstart=No
 
         if cpt % print_period == 0:
             transp = get_Gamma(alpha, beta, u, v)
-            err = (torch.sum(transp) - b).norm(2).pow(2).detach().cpu().item()
+            err = (torch.sum(transp, dim=0) - b).norm(2).pow(2).detach().cpu().item()
 
         if err <= stopThr:
             loop = False
